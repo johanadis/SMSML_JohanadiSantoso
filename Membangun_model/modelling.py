91baac3772590a31bb5d86acfcad41a6aa03b859
johanadi <55266511+johanadis@users.noreply.github.com>
2025-06-14 16:50:20 +0700

    Update modelling.py

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -10,17 +10,20 @@ import mlflow
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+os.environ["LOKY_MAX_CPU_COUNT"] = "4"
 
 def train_and_log_model(model, model_name, X_train, X_test, y_train, y_test):
     with mlflow.start_run(run_name=model_name):
         # Menangani kebutuhan pelatihan khusus model
         if model_name == 'CatBoost':
-            cat_features_indices = [X_train.columns.get_loc(col) for col in categorical_cols if col in X_train.columns] if categorical_cols else []
+            cat_features_indices = [X_train.columns.get_loc(col) for col in categorical_cols if col in X_train.columns]
             model.fit(X_train, y_train, cat_features=cat_features_indices)
+            y_pred = model.predict(X_test)
+            y_proba = model.predict_proba(X_test)[:, 1]
         elif model_name == 'LightGBM':
             X_train_lgb = X_train.copy()
             X_test_lgb = X_test.copy()
-            for col in categorical_cols or []:
+            for col in categorical_cols:
                 if col in X_train.columns:
                     X_train_lgb[col] = X_train_lgb[col].astype('category')
                     X_test_lgb[col] = X_test_lgb[col].astype('category')
@@ -73,13 +76,14 @@ def train_and_log_model(model, model_name, X_train, X_test, y_train, y_test):
 if __name__ == "__main__":
     # Untuk pengujian lokal dengan server MLflow
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
-    # mlflow.set_tracking_uri("file:./mlruns")
+    
+    # Menetapkan nama eksperimen MLflow: Personality_Prediction,  untuk mencatat seluruh run/model yang akan dilatih
     mlflow.set_experiment("Personality_Prediction")
     
     # Memuat dan mengacak data
     df = pd.read_csv('personality_dataset_preprocessing.csv')
     
-    # Mendefinisikan variabel global untuk menyimpan nama-nama kolom bertipe kategorikal
+    # Mendefinisikan variabel untuk menyimpan nama-nama kolom bertipe kategorikal
     categorical_cols = ['Stage_fear', 'Drained_after_socializing']
     
     # Membagi data
